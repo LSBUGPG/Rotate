a7967561277fe804ecef8be0a3c79f9e3a1c44c4
Paul Sinnett <paul.sinnett@gmail.com>
2020-10-20 13:14:43 +0100

    Initial commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int orientation = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            orientation = orientation + 1;
+            if (orientation > 3)
+            {
+                orientation = 0;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f * orientation);
+    }
+}
